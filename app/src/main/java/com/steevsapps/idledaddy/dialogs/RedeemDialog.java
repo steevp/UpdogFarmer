506af1a4076fe6d65521612a0e84617e94976e8f
Steven Pledger <steev@yourewinner.com>
2017-07-25 17:31:46 -0700

    Add feature to activate a product key

---
+++ app/src/main/java/com/steevsapps/idledaddy/dialogs/RedeemDialog.java
@@ -0,0 +1,58 @@
+package com.steevsapps.idledaddy.dialogs;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import com.steevsapps.idledaddy.R;
+
+public class RedeemDialog extends DialogFragment {
+    private DialogListener callback;
+
+    public static RedeemDialog newInstance() {
+        return new RedeemDialog();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            callback = (DialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement DialogListener.");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        callback = null;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.redeem);
+        builder.setMessage(R.string.redeem_msg);
+        final View view = LayoutInflater.from(getActivity()).inflate(R.layout.redeem_dialog, null);
+        final EditText input = (EditText) view.findViewById(R.id.input);
+        builder.setView(view);
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (callback != null) {
+                    callback.onYesPicked(input.getText().toString());
+                }
+            }
+        });
+        return builder.create();
+    }
+}
