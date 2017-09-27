28e76f7a6a178693f92e0f7b16487eea3e5f0b0a
Steven Pledger <steev@yourewinner.com>
2017-09-27 09:39:18 -0700

    Make login timeout survive screen rotation

---
+++ app/src/main/java/com/steevsapps/idledaddy/fragments/TimeOutFragment.java
@@ -0,0 +1,63 @@
+package com.steevsapps.idledaddy.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import com.steevsapps.idledaddy.listeners.TimeOutListener;
+
+/**
+ * Timeout handler that survives screen rotation
+ */
+public class TimeOutFragment extends Fragment {
+    public final static String TAG = TimeOutFragment.class.getSimpleName();
+    private TimeOutListener callback;
+
+    private final static int TIMEOUT_MILLIS = 15000;
+    private final Handler timeoutHandler = new Handler();
+    private final Runnable timeoutRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (callback != null) {
+                callback.onTimeOut();
+            }
+        }
+    };
+
+    public static TimeOutFragment newInstance() {
+        return new TimeOutFragment();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            callback = (TimeOutListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement TimeOutListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        callback = null;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        // Start timeout
+        timeoutHandler.postDelayed(timeoutRunnable, TIMEOUT_MILLIS);
+    }
+
+    @Override
+    public void onDestroy() {
+        // Stop timeout
+        timeoutHandler.removeCallbacks(timeoutRunnable);
+        super.onDestroy();
+    }
+}
