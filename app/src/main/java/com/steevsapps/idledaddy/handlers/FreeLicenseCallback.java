e2b2290c68ea8b2061c6031e38c7b2d495120e30
Steven Pledger <steev@yourewinner.com>
2017-09-29 16:30:02 -0700

    Allow activating free games

---
+++ app/src/main/java/com/steevsapps/idledaddy/handlers/FreeLicenseCallback.java
@@ -0,0 +1,36 @@
+package com.steevsapps.idledaddy.handlers;
+
+import uk.co.thomasc.steamkit.base.generated.SteammessagesClientserver2;
+import uk.co.thomasc.steamkit.base.generated.steamlanguage.EResult;
+import uk.co.thomasc.steamkit.steam3.steamclient.callbackmgr.CallbackMsg;
+import uk.co.thomasc.steamkit.types.JobID;
+
+public class FreeLicenseCallback extends CallbackMsg {
+    private JobID jobId;
+    private EResult result;
+    private int[] grantedApps;
+    private int[] grantedPackages;
+
+    FreeLicenseCallback(JobID jobID, SteammessagesClientserver2.CMsgClientRequestFreeLicenseResponse msg) {
+        this.jobId = jobID;
+        result = EResult.f(msg.eresult);
+        grantedApps = msg.grantedAppids;
+        grantedPackages = msg.grantedPackageids;
+    }
+
+    public JobID getJobId() {
+        return jobId;
+    }
+
+    public EResult getResult() {
+        return result;
+    }
+
+    public int[] getGrantedApps() {
+        return grantedApps;
+    }
+
+    public int[] getGrantedPackages() {
+        return grantedApps;
+    }
+}
