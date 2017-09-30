713ce7180d9705d60fc1ffb4536b4bb20f8aea6b
Steven Pledger <steev@yourewinner.com>
2017-09-29 19:20:20 -0700

    Update about dialog

--- app/src/main/java/com/steevsapps/idledaddy/dialogs/AboutDialog.java
+++ app/src/main/java/com/steevsapps/idledaddy/dialogs/AboutDialog.java
@@ -2,6 +2,7 @@ package com.steevsapps.idledaddy.dialogs;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -20,7 +21,8 @@ public class AboutDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final WebView webView = (WebView) LayoutInflater.from(getActivity()).inflate(R.layout.about_dialog, null);
         webView.loadUrl("file:///android_asset/about.html");
-        return new AlertDialog.Builder(getActivity(), R.style.Theme_AppCompat_Light_Dialog_Alert)
+        webView.setBackgroundColor(Color.TRANSPARENT);
+        return new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.pref_about)
                 .setView(webView)
                 .setPositiveButton(R.string.ok, null)
