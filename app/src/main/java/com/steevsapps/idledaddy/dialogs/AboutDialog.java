fe54e3f488fd4f00ffbdecddefde94a4c8841850
Steven Pledger <steev@yourewinner.com>
2017-12-31 14:56:23 -0800

    Improve about page loading

--- app/src/main/java/com/steevsapps/idledaddy/dialogs/AboutDialog.java
+++ app/src/main/java/com/steevsapps/idledaddy/dialogs/AboutDialog.java
@@ -11,6 +11,9 @@ import android.webkit.WebView;
 
 import com.steevsapps.idledaddy.R;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 public class AboutDialog extends DialogFragment {
@@ -25,20 +28,17 @@ public class AboutDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final WebView webView = (WebView) LayoutInflater.from(getActivity()).inflate(R.layout.about_dialog, null);
         final String lang = Locale.getDefault().getDisplayLanguage();
-        switch (lang) {
-            case "ru":
-                webView.loadUrl("file:///android_asset/about-ru.html");
-                break;
-            case "de":
-                webView.loadUrl("file://android_asset/about-de.html");
-                break;
-            case "uk":
-                webView.loadUrl("file://android_asset/about-uk.html");
-                break;
-            default:
-                webView.loadUrl("file:///android_asset/about.html");
-                break;
+        String uri = "file:///android_asset/about.html";
+        try {
+            // Load language-specific version of the about page if available.
+            final List<String> assets = Arrays.asList(getResources().getAssets().list(""));
+            if (assets.contains(String.format("about-%s.html", lang))) {
+                uri = String.format("file:///android_asset/about-%s.html", lang);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        webView.loadUrl(uri);
         webView.setBackgroundColor(Color.TRANSPARENT);
         return new AlertDialog.Builder(getActivity())
                 .setTitle(R.string.about)
