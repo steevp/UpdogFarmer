86559683d0188fb6c32e0a27a3c2eb5bcb49cabf
Steven Pledger <steev@yourewinner.com>
2018-06-18 07:19:49 -0700

    Add option to use a custom login id

--- app/src/main/java/com/steevsapps/idledaddy/Secrets.java
+++ app/src/main/java/com/steevsapps/idledaddy/Secrets.java
@@ -2,4 +2,5 @@ package com.steevsapps.idledaddy;
 
 public class Secrets {
     public final static String API_KEY = "***REMOVED***";
+    public final static int CUSTOM_OBFUSCATION_MASK = 0xB16B00B5;
 }
