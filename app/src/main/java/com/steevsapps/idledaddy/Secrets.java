3a145649dc566e262ca1355f7f3c1b81cac9ee94
Steven Pledger <steev@yourewinner.com>
2018-08-01 23:54:37 -0700

    :bike:

--- app/src/main/java/com/steevsapps/idledaddy/Secrets.java
+++ app/src/main/java/com/steevsapps/idledaddy/Secrets.java
@@ -2,5 +2,5 @@ package com.steevsapps.idledaddy;
 
 public class Secrets {
     public final static String API_KEY = "***REMOVED***";
-    public final static int CUSTOM_OBFUSCATION_MASK = 0xB16B00B5;
+    public final static int CUSTOM_OBFUSCATION_MASK = 0xF00DBAAD;
 }
